4ae40c38ebdb2d2497829b07c5211eed6de825b1
Tarun Velagala <tarunvelagala80@gmail.com>
2025-05-31 19:18:02 +0000

    Initialize SpringBoot Application boilerplate https://tinyurl.com/vtkr-prioritix-initializr

---
+++ src/main/java/com/vtkr/PrioritiX/PrioritiXApplication.java
@@ -0,0 +1,13 @@
+package com.vtkr.PrioritiX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrioritiXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrioritiXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtkr/PrioritiX/PrioritiXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtkr.PrioritiX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrioritiXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
