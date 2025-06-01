487de408687855923c6eccfc93139ac5e7f37f91
Tarun Velagala <tarunvelagala80@gmail.com>
2025-06-01 05:36:17 +0000

    Adds java-doc to TableName class

--- src/main/java/com/vtkr/prioritix/constants/TableName.java
+++ src/main/java/com/vtkr/prioritix/constants/TableName.java
@@ -2,11 +2,17 @@ package com.vtkr.prioritix.constants;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility class to hold table name constants used in JPA entity mappings.
+ * <p>
+ * This helps avoid hardcoded strings in annotations like
+ * {@code @Table(name = ...)}
+ * and ensures consistency across the codebase.
+ */
 @UtilityClass
 public class TableName {
-    
     /**
-     * Tasks table name.
+     * Name of the database table that stores Task entities.
      */
     public final String TASKS = "tasks";
 }
